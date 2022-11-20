1790e8801e0e7f967866926ef2f452ce6616d30b
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-11-20 10:09:41 -0300

    UseState Added

--- src/pages/GitSearch/index.tsx
+++ src/pages/GitSearch/index.tsx
@@ -1,16 +1,42 @@
 import Navbar from "components/Navbar";
 import ResultContent from "components/ResultContent";
+import { useState } from "react";
 import "./styles.css";
 
+// Objeto que representa os dados do formulário
+type FormData = {
+  login: string;
+}
+
 const GitSearch = () => {
+
+  /*
+    1 - formData: variável que ponta para o estado dos dados que está armazenando
+    2 - setFormData: função que altera o estado do "formData"
+    3 - useState<FormData>: fala que estamos usando o tipo de obj FormData
+    4 - FormData.login = inicia string vazia
+  */
+  const [formData, setFormData] = useState<FormData>({
+    login: "",
+  });
+
   // Sempre que mudar o conteúdo do input dispara a função
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("Conteúdo do input mudou para " + event.target.value);
+    const name = event.target.name; // Pegou o nome do input que no caso é login
+    const value = event.target.value; // Pegou o value do event que que no caso é o que foi digitado
+
+    /* 
+      1- Cria um objeto pegando tudo que já tinha no formData
+      2 - acessa o "name" desse objeto que vai corresponder ao atributo "login"
+      3 - coloca dentro desse atributo o "value" 
+    */
+    setFormData({ ...formData, [name]:value})
+
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log('Clicou no botão');
+    console.log(formData);
   }
 
   return (
@@ -23,6 +49,8 @@ const GitSearch = () => {
             <input
               id="search-text"
               type="text"
+              name="login"
+              value={formData.login}
               placeholder="Usuário GitHub"
               onChange={handleChange}
             />
